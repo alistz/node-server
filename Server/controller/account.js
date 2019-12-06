df2d3270377477adad775c0bf2cebac84b353735
alistar <alistz@163.com>
2019-12-06 10:03:52 +0800

    init repository

---
+++ Server/controller/account.js
@@ -0,0 +1,12 @@
+// controller/account.js
+
+exports.userById = userById;
+exports.createUser = createUser;
+
+function userById(req, res) {
+    res.end('滴滴答答滴滴答答滴滴答答滴滴答答滴滴答答滴滴答答滴滴答答滴滴答答.');
+}
+
+function createUser(req, res) {
+    res.end('waiting for impl.');
+}
